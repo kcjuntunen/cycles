f5153739032a5c23cd74eb12b2a4b8466c31968e
K. C. Juntunen <kjuntunen@fedora.amstore.com>
2016-12-13 13:54:29 -0500

    machinesetup test

---
+++ cycles/tests/test_machinesetup.py
@@ -0,0 +1,9 @@
+from unittest import TestCase
+from ..machinesetup import MachineSetup
+
+
+class TestCreate(TestCase):
+    def test_machine_setup(self):
+        program_name = '38288A'
+        ms = MachineSetup(program_name)
+        self.assertEqual(ms.program, '{} Setup'.format(program_name))
