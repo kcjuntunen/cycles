d0c2ba742b23e502bc643cecc109cfd0416ab067
K. C. Juntunen <kjuntunen@fedora.amstore.com>
2016-12-13 19:01:14 -0500

    update test for new functionality

--- cycles/tests/test_machinesetup.py
+++ cycles/tests/test_machinesetup.py
@@ -6,4 +6,4 @@ class TestCreate(TestCase):
     def test_machine_setup(self):
         program_name = '38288A'
         ms = MachineSetup(program_name)
-        self.assertEqual(ms.program, '{} Setup'.format(program_name))
+        self.assertEqual(ms.display_name, '{} Setup'.format(program_name))
